771ae006267ead6caa99e03b11b8013f742ee04b
Marco Miranda <marco.miranda@outlook.com>
2016-09-08 14:09:10 -0600

    Controller's and route's specs rebuilded
    - Controller specs reworked
    - Routing specs relocated
    - RecordNotFoundError added
    - person/show now renders an 404 when an invalid id is provided

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -20,140 +20,36 @@ require 'rails_helper'
 
 RSpec.describe PeopleController, type: :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Person. As you add validations to Person, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  describe "GET show.json" do
+    let!(:person) { create(:person) }
+    subject(:show_person) { get :show, params: { id: person.id, format: :json } }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+    it "assigns @person " do
+      show_person
+      expect(assigns(:person)).to be_a(Person)
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PeopleController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "assigns all people as @people" do
-      person = Person.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(assigns(:people)).to eq([person])
-    end
-  end
-
-  describe "GET #show" do
-    it "assigns the requested person as @person" do
-      person = Person.create! valid_attributes
-      get :show, params: {id: person.to_param}, session: valid_session
-      expect(assigns(:person)).to eq(person)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new person as @person" do
-      get :new, params: {}, session: valid_session
-      expect(assigns(:person)).to be_a_new(Person)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested person as @person" do
-      person = Person.create! valid_attributes
-      get :edit, params: {id: person.to_param}, session: valid_session
-      expect(assigns(:person)).to eq(person)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Person" do
-        expect {
-          post :create, params: {person: valid_attributes}, session: valid_session
-        }.to change(Person, :count).by(1)
-      end
-
-      it "assigns a newly created person as @person" do
-        post :create, params: {person: valid_attributes}, session: valid_session
-        expect(assigns(:person)).to be_a(Person)
-        expect(assigns(:person)).to be_persisted
-      end
-
-      it "redirects to the created person" do
-        post :create, params: {person: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(Person.last)
+      %w(id name last_name_1 last_name_2 gender state city district).each do |field|
+        expect(assigns(:person).send(field)).to eq(person.send(field))
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved person as @person" do
-        post :create, params: {person: invalid_attributes}, session: valid_session
-        expect(assigns(:person)).to be_a_new(Person)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, params: {person: invalid_attributes}, session: valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested person" do
-        person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: new_attributes}, session: valid_session
-        person.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "assigns the requested person as @person" do
-        person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: valid_attributes}, session: valid_session
-        expect(assigns(:person)).to eq(person)
-      end
-
-      it "redirects to the person" do
-        person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(person)
-      end
+    it "renders the template show.json.jbuilder" do
+      expect(show_person).to be_successful
+      expect(show_person).to render_template("people/show")
     end
 
-    context "with invalid params" do
-      it "assigns the person as @person" do
-        person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: invalid_attributes}, session: valid_session
-        expect(assigns(:person)).to eq(person)
+
+    context "when an invalid id is provided" do
+      subject(:show_person) { get :show, params: { id: 0, format: :json} }
+
+      it "returns an error code" do
+        show_person
+        expect(response).to have_http_status(404)
       end
 
-      it "re-renders the 'edit' template" do
-        person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: invalid_attributes}, session: valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested person" do
-      person = Person.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: person.to_param}, session: valid_session
-      }.to change(Person, :count).by(-1)
     end
 
-    it "redirects to the people list" do
-      person = Person.create! valid_attributes
-      delete :destroy, params: {id: person.to_param}, session: valid_session
-      expect(response).to redirect_to(people_url)
-    end
   end
 
 end

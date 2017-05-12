79b4535694b290f3d23c6d949add61fbf25e2c2a
Esteban Muñoz <esteban.ucr@gmail.com>
2017-05-12 17:55:18 -0600

    Simple authentication for the API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,28 @@
 class ApplicationController < ActionController::API
+  class NotAuthorizedError < Exception; end
+
+  before_action :authenticate
+
+  rescue_from NotAuthorizedError, with: :not_authorized
   rescue_from ApplicationRecord::RecordNotFoundError, with: :render_404
+
+  private
+
+  def not_authorized
+    render json: { status: 403, message: "Not authorized" }
+  end
 
   def render_404
     render json: { status: 404, message: "Not found" }
+  end
+
+  def authenticate
+    # Having a single access token should be good enough for now,
+    # as this API is not intended to be used by different clients (for now)
+    if params[:access_token] != Rails.application.secrets.access_token
+      raise NotAuthorizedError, "Invalid access token"
+    end
+
+    true
   end
 end

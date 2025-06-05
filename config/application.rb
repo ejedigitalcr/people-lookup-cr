3f463510c060805c71ba6ad4cc738f43faba2fc4
Esteban Muñoz Solís <esteban.ucr@gmail.com>
2025-06-05 08:37:46 -0600

    Ruby and Rails upgrade

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,12 @@ Bundler.require(*Rails.groups)
 module Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    #config.load_defaults 5.0
-    config.active_record.legacy_connection_handling = false
+    config.load_defaults 7.2
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
